d9574540f226d187be163916389cfd2777bcc7c0
amazing.gao <amazing.gao@qq.com>
2021-09-07 10:23:55 +0800

    build(vue): fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-tools' : '/my-tools'
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-tools/' : '/my-tools/'
 }
